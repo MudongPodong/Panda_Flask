02b92d68b76955bd43221624c24b8b6c847d9050
SeungHyeon Lee <diqzk1562@naver.com>
2023-06-05 16:09:03 +0900

    챗봇 로직 등 추가

--- PandaServer2.py
+++ PandaServer2.py
@@ -2,6 +2,16 @@ import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules
 from flask import Flask, request,jsonify
+import numpy
+import tensorflow
+import random
+import json
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+from tensorflow import keras
+nltk.download('punkt')
+stemmer = LancasterStemmer()
+
 
 app = Flask(__name__)
 
@@ -19,8 +29,6 @@ def receiveData():
     # print(recData)
     # print(recData['myList'])
     # print(recData['otherList']['b1234567@email.com'])
-
-
 
     # 'otherList' 값 가져오기
     myList= recData['myList']
@@ -80,6 +88,92 @@ def receiveData():
     }
     return jsonify(data)
 
+@app.route('/chatbot',methods=['GET','POST'])
+def chatbotData():
+    with open('intents.json', 'rb') as file:
+        file_content = file.read().decode('utf-8')
+
+    data = json.loads(file_content)
+
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data['intents']:
+        for pattern in intent['patterns']:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent['tag'] not in labels:
+            labels.append(intent['tag'])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    tensorflow.compat.v1.reset_default_graph()
+    model = keras.models.load_model("model.h5")
+
+    inp = request.get_data(as_text=True)
+    print('전달된 문자열:', inp)
+
+    results = model.predict([bag_of_words(inp, words)])
+
+    max_prob = max(results[0])
+
+    if max_prob < 0.5:
+        return '이해가 가질 않습니다'
+    else:
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        message = random.choice(responses)
+        print(message)
+
+        return message
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag).reshape(1, -1)
 
 if __name__ == '__main__':
     app.run()
